269340e9129919276233a265cd24e2c1c337bb06
liniefrancisco <liniefrancisco01@gmail.com>
2024-07-11 08:35:38 +0800

    Delete Application1

--- application1/helpers/additional_helper.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-if(!function_exists('getCurrentDate')){
-	function getCurrentDate($format = 'Y-m-d'){
-		return date($format);
-	}
-}
--- application1/views/maintenance/maintenance.php
+++
@@ -1,1 +0,0 @@
-<center><img src="<?php echo base_url();?>img/maintenance.jpg" alt="maintenance" class="center"></center>
